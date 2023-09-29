0825ca026ac2beff0e649cde89181c139d3b68a5
Alysson tschurts <t.alyson@alunoescolassociais.org.br>
2023-09-29 11:49:15 -0300

    função para preencher formulario

--- controller.js
+++ controller.js
@@ -7,3 +7,17 @@ const limparformulario = () => {
     document.getElementById('cidade').value = '';
     document.getElementById('estado').value = '';
 }
+
+// Verifica se o CEP é valido
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero); //testa numero informado com expressão regular
+const cepValido = (cep) => cep.lenght == 8 && eNumero(cep); // verifica tamanho do cep digitado e executa função de validação do cep eNumero
+
+//função para preencher formulario
+const preencherformulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+
+}
